decf91c01ba07f71d0f6cafb9eb27c715e54b831
Baptiste Prévost <baptiste.prevost@telecom-paris.fr>
2021-12-02 01:33:39 +0100

    Solved

--- COMMEDIA/COMMEDIA.cpp
+++ COMMEDIA/COMMEDIA.cpp
@@ -3,10 +3,11 @@
 
     Idea : 
         - The idea is the same as the 2D Lloyd's puzzle. It is about the parity of the permutations.
-        - You can see the cube as a 3D chessboard, to highlight the parity of the initial permutation
-        - The initial position of the 0 cube in the puzzle allows us to calculate if reaching this permutations necessitated would have used an odd or even number of transpositions.
+        - You can see the cube as a 3D chessboard with white and black boxes (even and odd), to highlight the parity of the initial permutation
+        - In the initial permutation, we use the position of the empty cube (0) to calculate the number of moves (transpositions) necessary to reach this permutation from the base one.
+        - The parity of this number is the signature of a reachable permutation with the empty cube located here. This signature is independant from the moves (=transpositions) we decided to perform to obtain a permutation from another.
         - Then, by counting the length of each cycle in the initial  puzzle, we evaluate the signature of the initial permutation.
-        - The puzzle is solvable if and only if this two informations are compatible
+        - The puzzle is solvable if and only if this two signatures are compatible.
 */
 
 #include <iostream>
@@ -26,7 +27,7 @@ int main() {
                     cin >> cubes[z*M*M + y*M + x];
                     done[z*M*M + y*M + x] = false;
                     if(cubes[z*M*M + y*M + x] == 0) {
-                        parity = (x+y+z)%2;
+                        parity = (M-1-x + M-1-y + M-1-z)%2;
                         cubes[z*M*M + y*M + x] = M*M*M; //replace 0 with M*M*M
                     }
                 }
