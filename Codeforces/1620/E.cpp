faea2c2dbf12f6aba91c0d6177e07ee3a3b47eb1
Baptiste Prévost <baptiste.prevost@telecom-paris.fr>
2021-12-21 23:40:02 +0100

    Clean answer on E

--- Codeforces/1620/E.cpp
+++ Codeforces/1620/E.cpp
@@ -2,41 +2,47 @@
     Author : Baptiste Prévost
 
     Idea : 
-        - 
-        -
+        - Store and read the instructions in the inversed order
 */
 
 #include <iostream>
 #include <map>
+#include <utility>
 #include <list>
-#include <algorithm>
 using namespace std;
 
 int main() {
+    int n, a, b, c;
+    list<int> res;
+    int value[500001];
+    pair<int, int> instructions[500001];
 
-    list<pair<int, int> > history[500001];
-    list<pair<int, int> > seq;
-    for(int i = 1; i < 500001; i++) {
-        history[i].push_back(make_pair(0, i));
+    for(int i=0; i<500001; i++) {
+        value[i]=i;
     }
-    
-    int q, a, x, y;
-    for(int i=0; i<q; i++){
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
         cin >> a;
         if(a==1) {
-            cin >> x;
-            seq.push_back(make_pair(x, i));
+            cin >> b;
+            c=0;
         } else {
-            cin >> x >> y;
-            history[x].push_back(make_pair(q, y));
+            cin >> b >> c;
+        }
+        instructions[n-i-1] = make_pair(b,c);
+    }
+
+
+    for(int i = 0; i<n; i++) {
+        if(instructions[i].second == 0) {
+            res.push_front(value[instructions[i].first]);
+        } else {
+            value[instructions[i].first] = value[instructions[i].second];
         }
     }
 
-    for(list<pair<int, int> >::iterator it = seq.begin(); it != seq.end(); it++) {
-        int ret = it->first;
-        list<pair<int, int> >::iterator it2 = history[ret].end();
-        while() {
-            if(it2 == history[ret].begin()) break;
-        }
+    for(list<int>::iterator it = res.begin(); it != res.end(); it++) {
+        cout << *it << ' ';
     }
 }
